065bf6c4a024c4c640ccbdb27f8f158da49feb19
James <33908344+allen-munsch@users.noreply.github.com>
2023-03-29 16:15:44 -0500

    Update hey_gpt_hey_whisper.sh

--- hey_gpt_hey_whisper.sh
+++ hey_gpt_hey_whisper.sh
@@ -1,4 +1,5 @@
 
+
 # Function to handle the SIGINT signal (Ctrl+C)
 handle_ctrl_c() {
     echo "Recording stopped. Processing the audio..."
@@ -8,8 +9,14 @@ handle_ctrl_c() {
 
 # Function to call the OpenAI API
 hey_gpt() {
-    json_payload=$(jq -n --arg prompt "$1 $2" '{"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": $prompt}], "temperature": 0.7}')
-    curl https://api.openai.com/v1/chat/completions -s \
+    item="$1 $2"
+    if [ "$2" == "clippy" ]; then
+        item="$1 $(pbpaste)"
+    fi
+    echo "$item"
+    json_payload=$(jq -n --arg prompt "$item" '{"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": $prompt}], "temperature": 0.7}')
+    echo "$json_payload"
+    curl -Lv https://api.openai.com/v1/chat/completions -s \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $OPENAI_API_KEY" \
     -d "$json_payload" | jq -r '.choices[0].message.content'
